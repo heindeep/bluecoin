c292e9bebd52e5a87f84ea70b1b35b583eb10334
heindeep <heindeep15@googlemail.com>
2019-03-15 17:23:41 +0100

    Update gitian-build.sh

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -17,7 +17,7 @@ osx=true
 SIGNER=
 VERSION=
 commit=false
-url=https://github.com/snodeco/snode-coin.git
+url=https://github.com/heindeep/bluecoin.git
 proc=2
 mem=2000
 lxc=true
@@ -39,7 +39,7 @@ version		Version number, commit, or branch to build. If building a commit or bra
 
 Options:
 -c|--commit	Indicate that the version argument is for a commit or branch
--u|--url	Specify the URL of the repository. Default is https://github.com/bluecoin-project/bluecoin
+-u|--url	Specify the URL of the repository. Default is https://github.com/heindeep/bluecoin
 -v|--verify 	Verify the gitian build
 -b|--build	Do a gitian build
 -s|--sign	Make signed binaries for Windows and Mac OSX
@@ -237,8 +237,8 @@ echo ${COMMIT}
 if [[ $setup = true ]]
 then
     sudo apt-get install ruby apache2 git apt-cacher-ng python-vm-builder qemu-kvm qemu-utils
-    git clone https://github.com/snodeco/gitian.sigs.git
-    git clone https://github.com/snodeco/bluecoin-detached-sigs.git
+    git clone https://github.com/heindeep/gitian.sigs.git
+    git clone https://github.com/heindeep/bluecoin-detached-sigs.git
     git clone https://github.com/devrandom/gitian-builder.git
     pushd ./gitian-builder
     if [[ -n "$USE_LXC" ]]
